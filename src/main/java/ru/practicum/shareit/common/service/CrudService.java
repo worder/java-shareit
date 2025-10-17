685bd588f6d4cd4a27006f3c13ec99a1793c758f
worder <worderrus@yandex.com>
2025-10-17 07:16:41 +0700

    fix: use generic type

--- src/main/java/ru/practicum/shareit/common/service/CrudService.java
+++ src/main/java/ru/practicum/shareit/common/service/CrudService.java
@@ -3,9 +3,9 @@ package ru.practicum.shareit.common.service;
 public interface CrudService<ID, D, C, U> {
     D create(C request);
 
-    D read(Long id);
+    D read(ID id);
 
-    D update(Long id, U request);
+    D update(ID id, U request);
 
     void delete(ID id);
 }
